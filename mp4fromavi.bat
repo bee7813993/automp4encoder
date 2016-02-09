1deddb0f542ddbebd84b6f75c53292aa5c406697
bee7813993 <bee@mail.pcgame-r18.jp>
2016-02-10 02:42:51 +0900

    avi以外の動画ファイルに対応
    DirectShowSourceで読める形式はすべて対応(mp4,mpg等)
    avs形式は未対応

--- mp4fromavi.bat
+++ mp4fromavi.bat
@@ -13,14 +13,31 @@ set CMDX264="%~dp0\x264.2665.x86.exe"
 set CMDAACENC="%~dp0\neroAacEnc.exe"
 set CMDMP4BOX="%~dp0\MP4Box.exe"
 
-echo AVISource("%~1") 1>%AVSFILE%
-if not "%2" == ""  echo TextsubMod("%~2") 1>>%AVSFILE%
+
+if "%~x1" == ".avi" (
+  call :aviread "%~1"
+) else (
+  call :dsread "%~1"
+)
+if not "%2" == "" (
+  if "%~x2" == ".ass" (
+    call :assread2 "%~2"
+  )
+)
+
 echo return last 1>>%AVSFILE%
 
-%CMDAVSWAV% %AVSFILE% %TMPAUDIOWAVFILE%
 
+%CMDAVSWAV% %AVSFILE% %TMPAUDIOWAVFILE%
 %CMDX264% -q %VIDEOQUALITY% --threads auto --output %TMPVIDEONOAUDIOFILE%  %AVSFILE%
 %CMDAACENC% -q %AUDIOQUALITY% -ignorelength -if %TMPAUDIOWAVFILE% -of %TMPAUDIOM4AFILE%
+set COUNT=1
+call :get_extension %VIDEOFINISHFILE%
+set EXT=%extension%
+set FINFILE=%filebody%
+IF EXIST %VIDEOFINISHFILE% (
+    call :setvideofinishfile "%FINFILE%(%COUNT%)%EXT%"
+)
 %CMDMP4BOX% -add %TMPVIDEONOAUDIOFILE% -add %TMPAUDIOM4AFILE% -new %VIDEOFINISHFILE%
 del %AVSFILE%
 del %TMPAUDIOWAVFILE%
@@ -29,3 +46,29 @@ del %TMPVIDEONOAUDIOFILE%
 
 echo エンコード完了！！！
 pause
+exit
+
+:aviread
+echo AVISource("%~1") 1>%AVSFILE%
+exit /b
+
+:dsread
+echo DirectShowSource("%~1") 1>%AVSFILE%
+exit /b
+
+:assread2
+echo TextsubMod("%~1") 1>>%AVSFILE%
+exit /b
+
+:setvideofinishfile
+set VIDEOFINISHFILE="%FINFILE%(%COUNT%)%EXT%"
+exit /b
+
+
+:get_extension
+set extension=%~x1
+rem set extension=%extension:~1%
+set filebody=%~dpn1
+rem set filebody=%filebody:~1%
+exit /b
+
